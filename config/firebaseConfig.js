fac1d61c3a5bbdb97d8f803004b4482aa34a9834
devysf <ozacetyusuf@gmail.com>
2019-02-04 19:41:16 +0300

    .gitignore is now working

--- config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD2np5C0C16dh46rGZugUtv9lRHoUgkX4U",
-  authDomain: "ecoshop-c0838.firebaseapp.com",
-  databaseURL: "https://ecoshop-c0838.firebaseio.com",
-  projectId: "ecoshop-c0838",
-  storageBucket: "ecoshop-c0838.appspot.com",
-  messagingSenderId: "469877786437"
-};
