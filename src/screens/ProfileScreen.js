c980212fdabd93df4d25f0c2e035917b73eb56d5
devysf <ozacetyusuf@gmail.com>
2019-02-13 18:59:14 +0300

    Styling of profile page is designed.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,16 +1,19 @@
+// DO: users can edit your profile photo
+//Also, can edit,delete your item
 import React, { Component } from "react";
 import {
+  ScrollView,
+  StyleSheet,
   View,
   Text,
   ListView,
-  Image,
-  TouchableWithoutFeedback
+  Image
 } from "react-native";
 
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
-import { Button } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 
 class ProfileScreen extends Component {
   componentWillMount() {
@@ -33,14 +36,24 @@ class ProfileScreen extends Component {
 
   renderRow(item) {
     return (
-      <View>
+      <View style={styles.item}>
         <Text>{item.name}</Text>
         <View>
           <Image
             source={{ uri: item.image }}
-            style={{ width: 250, height: 250 }}
+            style={{ width: 100, height: 100 }}
           />
         </View>
+      </View>
+    );
+  }
+
+  renderRow2(item) {
+    return (
+      <View style={styles.item}>
+        <Card title={item.name ? item.name : null} image={{ uri: item.image }}>
+          <Text style={{ marginBottom: 10 }}>{item.description}</Text>
+        </Card>
       </View>
     );
   }
@@ -51,17 +64,82 @@ class ProfileScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>ProfileScreen</Text>
-        <ListView
-          enableEmptySections
-          dataSource={this.dataSource}
-          renderRow={this.renderRow}
+      <ScrollView style={styles.container}>
+        <View style={styles.header} />
+
+        <Image
+          style={styles.avatar}
+          source={{ uri: "https://bootdey.com/img/Content/avatar/avatar6.png" }}
         />
-      </View>
+
+        <View style={styles.body}>
+          <View style={styles.bodyContent}>
+            <Text style={styles.name}>John Doe</Text>
+          </View>
+        </View>
+
+        <View>
+          <ListView
+            contentContainerStyle={styles.list}
+            enableEmptySections
+            dataSource={this.dataSource}
+            renderRow={this.renderRow2}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  item: {
+    margin: 5,
+    width: 150
+  },
+  header: {
+    backgroundColor: "#ADD8E6",
+    height: 100
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 50
+  },
+  name: {
+    fontSize: 22,
+    color: "#FFFFFF",
+    fontWeight: "600"
+  },
+  body: {
+    marginTop: 40
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: "center",
+    padding: 30
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#00BFFF"
+  }
+});
 
 const mapStateToProps = state => {
   return {
